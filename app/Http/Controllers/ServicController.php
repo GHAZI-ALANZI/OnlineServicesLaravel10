a66692e064507439ca608342dee9f33591f2b120
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2024-01-20 15:51:44 +0100

    prepare serviceController

--- app/Http/Controllers/ServicController.php
+++ app/Http/Controllers/ServicController.php
@@ -10,56 +10,99 @@ class ServicController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        $services = Service::latest()->paginate(5);
+        
+        return view('services.index',compact('services'))
+                    ->with('i', (request()->input('page', 1) - 1) * 5);
     }
-
+  
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('services.create');
     }
-
+  
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+    
+        $input = $request->all();
+    
+        if ($image = $request->file('image')) {
+            $destinationPath = 'images/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }
+      
+        Service::create($input);
+       
+        return redirect()->route('services.index')
+                        ->with('success','services created successfully.');
     }
-
+  
     /**
      * Display the specified resource.
      */
-    public function show(Service $service)
+    public function show(Service $service): View
     {
-        //
+        return view('services.show',compact('service'));
     }
-
+  
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Service $service)
+    public function edit(Service $service): View
     {
-        //
+        return view('services.edit',compact('service'));
     }
-
+  
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Service $service)
+    public function update(Request $request, Service $service): RedirectResponse
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+    
+        $input = $request->all();
+    
+        if ($image = $request->file('image')) {
+            $destinationPath = 'images/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }else{
+            unset($input['image']);
+        }
+            
+        $service->update($input);
+      
+        return redirect()->route('services.index')
+                        ->with('success','service updated successfully');
     }
-
+  
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Service $service)
+    public function destroy(Service $service): RedirectResponse
     {
-        //
+        $service->delete();
+         
+        return redirect()->route('services.index')
+                        ->with('success','service deleted successfully');
     }
 }
