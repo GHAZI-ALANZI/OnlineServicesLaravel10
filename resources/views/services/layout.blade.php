aaee52bc95cf5e1143e3ccc704262cdab1cf1333
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2024-01-21 11:34:55 +0100

    testing app and add some styles

--- resources/views/services/layout.blade.php
+++ resources/views/services/layout.blade.php
@@ -13,7 +13,7 @@
 
 <!-- Start Nav -->
 <div class="card mb-3">
-  <img  style="max-height:20rem;" src="https://cdn.pixabay.com/photo/2017/07/25/09/39/call-center-2537390_1280.jpg" class="img-fluid" alt="...">
+  <img  style="max-height:15rem;" src="https://cdn.pixabay.com/photo/2017/07/25/09/39/call-center-2537390_1280.jpg" class="img-fluid" alt="...">
 
 </div>
 
@@ -24,5 +24,95 @@
     </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>  
+
+<!-- Footer -->
+<footer class="bg-body-tertiary text-center">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Images -->
+    <section class="">
+      <div class="row">
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div data-mdb-ripple-init
+            class="bg-image hover-overlay shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img src="https://cdn.pixabay.com/photo/2016/06/03/12/42/popcorn-1433326_1280.jpg" class="w-100" />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div data-mdb-ripple-init
+            class="bg-image hover-overlay shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img src="https://cdn.pixabay.com/photo/2020/03/02/19/27/banking-4896672_1280.jpg" class="w-100" />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div data-mdb-ripple-init
+            class="bg-image hover-overlay shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img src="https://cdn.pixabay.com/photo/2021/02/03/00/10/receptionists-5975962_1280.jpg" class="w-100" />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div data-mdb-ripple-init
+            class="bg-image hover-overlay shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img src="https://cdn.pixabay.com/photo/2015/05/28/14/53/ux-788002_960_720.jpg" class="w-100" />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div data-mdb-ripple-init
+            class="bg-image hover-overlay shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img src="https://cdn.pixabay.com/photo/2015/06/10/16/36/mark-804938_960_720.jpg" class="w-100" />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div data-mdb-ripple-init
+            class="bg-image hover-overlay shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img src="https://cdn.pixabay.com/photo/2016/03/13/10/19/customer-1253483_1280.jpg" class="w-100" />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Section: Images -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4 " style="font-family: Monoton, sans-serif;
+  font-weight: 400;font-style: normal;">
+    © 2024 Copyright : Online Service
+    
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
 </body>
 </html>
